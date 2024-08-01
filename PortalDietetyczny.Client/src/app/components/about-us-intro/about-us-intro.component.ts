8e51c8fa2654fc2518f2ea4f322389a90465941b
Mateusz Miłkowski <mat.milkowski@gmail.com>
2024-08-01 04:46:31 +0200

    about update

--- PortalDietetyczny.Client/src/app/components/about-us-intro/about-us-intro.component.ts
+++ PortalDietetyczny.Client/src/app/components/about-us-intro/about-us-intro.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-us-intro',
   templateUrl: './about-us-intro.component.html',
   styleUrl: './about-us-intro.component.css'
 })
-export class AboutUsIntroComponent {
+export class AboutUsIntroComponent implements OnInit
+{
+  public isBigScreen: boolean = false;
+
+  ngOnInit(): void
+  {
+    window.addEventListener('resize', () => {
+      console.log(window.innerWidth);
+
+      this.isBigScreen = window.innerWidth <= 431;
+    });
+  }
 
 }
