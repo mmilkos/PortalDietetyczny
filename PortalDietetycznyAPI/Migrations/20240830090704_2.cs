260f3792ab30c5975ffc872687a89f2a6b5d3878
Mateusz Miłkowski <mat.milkowski@gmail.com>
2024-08-31 10:02:34 +0200

    Recipe display and sorting

---
+++ PortalDietetycznyAPI/Migrations/20240830090704_2.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortalDietetycznyAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class _2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Uid",
+                table: "Recipes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_Uid",
+                table: "Recipes",
+                column: "Uid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Recipes_Uid",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "Uid",
+                table: "Recipes");
+        }
+    }
+}
